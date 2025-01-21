a5559e80f62f9b1697d7b5081434f3d0b6a397cc
PedroLucasAraujo <pedrolpriv@gmail.com>
2025-01-21 17:02:04 -0300

    feat: create list all transactions test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,7 @@
 import { expect, it, beforeAll, afterAll, describe } from "vitest";
 import request from "supertest";
 import { app } from "../src/app";
+import { title } from "process";
 
 describe("Transactions Routes", () => {
   beforeAll(async () => {
@@ -19,5 +20,29 @@ describe("Transactions Routes", () => {
     });
 
     expect(response.statusCode).toEqual(201);
+  });
+
+  it("should be able to list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New transaction",
+        amount: 5000,
+      }),
+    ]);
   });
 });
